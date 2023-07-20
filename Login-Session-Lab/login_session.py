6c4dc5750505bd0c2baf6c12fee4f1222649ebc6
GuyWi24-meet <1963jackwilliams@gmail.com>
2023-07-20 16:29:31 +0300

    not finished quite yet

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,11 +4,10 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['GET', 'POST']) # What methods are needed?
 def home():
-	
-	return render_template('home.html')
 
+	return render_template('home.html')
 
 @app.route('/error')
 def error():
@@ -22,7 +21,7 @@ def display():
 	return render_template('display.html', ) # What variables are needed?
 
 
-@app.route('/thanks')
+@app.route('/thanks', methods=['GET', 'POST'])
 def thanks():
 
 	return render_template('thanks.html')
